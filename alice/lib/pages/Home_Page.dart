e49137624c854b9b3bef7955ef0addd2631bd30b
Anihara <shashikush9211@gmail.com>
2023-08-17 16:48:33 +0530

    Day 3 complete

--- alice/lib/pages/Home_Page.dart
+++ alice/lib/pages/Home_Page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final bool isFemale = false;
+  final bool city = true;
   final String name = 'Shashi Kushwaha';
   final num temp = 18;
   final String place = 'Shimla';
