6331f41a4de5213d926c9ef4b2fac253b0f3a5f0
Anihara <shashikush9211@gmail.com>
2023-08-17 15:11:14 +0530

    day 2 completed

---
+++ alice/lib/Home_Page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final bool isFemale = false;
+  final String name = 'Shashi Kushwaha';
+  final num temp = 18;
+  final String place = 'Shimla';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+              "Welcome to $name ki padhai bhaiyo, and I'm a Male living in $place at $temp degree celcious temperature"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
