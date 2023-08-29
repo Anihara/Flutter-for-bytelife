5958908eb229b546d3475a31f1185926969b12b9
Anihara <shashikush9211@gmail.com>
2023-08-29 23:03:31 +0530

    day 12 completed

--- alice/lib/pages/Home_Page.dart
+++ alice/lib/pages/Home_Page.dart
@@ -1,6 +1,5 @@
 import 'package:alice/models/Inventory.dart';
 import 'package:alice/widgets/drawer.dart';
-import 'package:alice/widgets/item_widget.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -36,22 +35,46 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Products"),
       ),
       body: Padding(
           padding: const EdgeInsets.all(5.0),
-          child: ListView.builder(
-            itemCount: CatalogModel.item.length,
-            // itemCount: dummyList.length,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1, mainAxisSpacing: 16, crossAxisSpacing: 16),
             itemBuilder: (context, index) {
-              return ItemWidget(
-                item: CatalogModel.item[index],
-                // item: dummyList[index],
+              final item = CatalogModel.item[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: GridTile(
+                  header: Container(
+                    child: Text(
+                      item.name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 200, 165, 138),
+                        border: Border()),
+                  ),
+                  child: Image.network(item.image, fit: BoxFit.cover),
+                  footer: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 220, 105, 34),
+                          border: Border()),
+                      child: Text(
+                        item.price.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                ),
               );
             },
+            itemCount: CatalogModel.item.length,
           )),
       drawer: MyDrawer(),
     );
